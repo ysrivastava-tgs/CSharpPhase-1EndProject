0ef0958d7b0b0b154757c6f8ad2036163dd00f67
yashasvi kumar srivastava <ysrivastava@teksystems.com>
2021-03-12 10:16:27 +0530

    Tecaher.cs is a class containing tecaher properties

---
+++ Teacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPhase_1EndProject
+{
+    public class Teacher
+    {
+        public int ID { get;  }
+        public string Name { get; }
+        public string ClassAndSection { get; }
+        public Teacher(int id,string nm,string cls)
+        {
+            ID = id;
+            Name = nm;
+            ClassAndSection = cls;
+        }
+
+        public override string ToString()
+        {
+            string str = ID.ToString() + "," + Name + "," + ClassAndSection;
+            return str;
+        }
+    }
+}
